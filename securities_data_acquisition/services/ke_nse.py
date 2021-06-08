5e79c732ce976f484d803cbda376c706a00bc2b9
Derick Mwenda <derickmwenda5@gmail.com>
2021-06-08 06:32:52 +0300

    Script to fetch live data to be run as a service implemented

---
+++ securities_data_acquisition/services/ke_nse.py
@@ -0,0 +1,30 @@
+"""
+Get the latest live data from Nairobi Stock Exhange.
+This script is meant to be run as a service so that it
+fetches data all the time as long as the server is up
+and running.
+"""
+# pyright: reportMissingImports=false
+import time
+import logging
+from pathlib import Path
+import sys
+
+# project_root = Path(__file__).parents[2].resolve()
+# sys.path.insert(0, str(project_root))
+
+# from securities_data_acquisition.fetch_latest import execute
+from fetch_latest import execute
+# import fetch_latest
+
+# TODO make the logger a module level logger
+logger = logging.getLogger("nse_fetch_latest")
+
+
+if __name__ == "__main__":
+    while True:
+        execute()
+        time.sleep(30)
+# while True:
+#     execute()
+#     time.sleep(30)
