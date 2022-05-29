7736bacabeb25f537d33a5834126247aff458323
SKagai <sallykagai@rocketmail.com>
2022-05-29 14:53:53 +0300

    PEP compliance

--- securities_data_acquisition/scrape_historical/scrape_historical/items.py
+++ securities_data_acquisition/scrape_historical/scrape_historical/items.py
@@ -24,35 +24,45 @@ def convert_to_float(string):
     except ValueError:
         return None
 
+
 def strip_comma(string):
     return re.sub(",", "", string)
+
 
 class SEItem(scrapy.Item):
     ticker_symbol = scrapy.Field(output_processor=TakeFirst())
     company_name = scrapy.Field(output_processor=TakeFirst())
-    date = scrapy.Field(input_processor=MapCompose(get_date), output_processor=TakeFirst())
-    price = scrapy.Field(input_processor=MapCompose(convert_to_float), output_processor=TakeFirst())
+    date = scrapy.Field(input_processor=MapCompose(get_date),
+                        output_processor=TakeFirst())
+    price = scrapy.Field(input_processor=MapCompose(convert_to_float),
+                         output_processor=TakeFirst())
 
 
 class MystockKeItem(scrapy.Item):
     ticker_symbol = scrapy.Field(output_processor=TakeFirst())
     date = scrapy.Field(output_processor=TakeFirst())
-    closing_price = scrapy.Field(input_processor=MapCompose(convert_to_float), output_processor=TakeFirst())
-    high_price = scrapy.Field(input_processor=MapCompose(convert_to_float), output_processor=TakeFirst())
-    low_price = scrapy.Field(input_processor=MapCompose(convert_to_float), output_processor=TakeFirst())
-    volume = scrapy.Field(input_processor=MapCompose(strip_comma, convert_to_float), output_processor=TakeFirst())
+    closing_price = scrapy.Field(input_processor=MapCompose(convert_to_float),
+                                 output_processor=TakeFirst())
+    high_price = scrapy.Field(input_processor=MapCompose(convert_to_float),
+                              output_processor=TakeFirst())
+    low_price = scrapy.Field(input_processor=MapCompose(convert_to_float),
+                             output_processor=TakeFirst())
+    volume = scrapy.Field(input_processor=MapCompose(strip_comma,
+                          convert_to_float), output_processor=TakeFirst())
+
 
 class KENSEItem(scrapy.Item):
     @staticmethod
     def process_isin_code(string):
         """Strip the 'ISIN CODE:' part to remain with the actual ISIN code"""
         return string[10:]
-    
+
     @staticmethod
     def process_ticker_symbol(string):
         """Strip the part 'Trading Symbol:' to remain with the actual symbol"""
         return string[15:]
-    
+
+    @staticmethod
     def process_company_name(string):
         """strip the share type part of the name
         for example: in Safaricom PLC Ord 0.05 return Safaricom PLC
@@ -64,6 +74,13 @@ class KENSEItem(scrapy.Item):
         else:
             return string
 
-    ticker_symbol = scrapy.Field(input_processor=MapCompose(process_ticker_symbol), output_processor=TakeFirst())
-    company_name = scrapy.Field(input_processor=MapCompose(process_company_name),output_processor=TakeFirst())
-    ISIN_CODE = scrapy.Field(input_processor=MapCompose(process_isin_code), output_processor=TakeFirst())
+    ticker_symbol = scrapy.Field(input_processor=MapCompose(
+                                                    process_ticker_symbol
+                                                ),
+                                 output_processor=TakeFirst())
+    company_name = scrapy.Field(input_processor=MapCompose(
+                                                    process_company_name
+                                                ),
+                                output_processor=TakeFirst())
+    ISIN_CODE = scrapy.Field(input_processor=MapCompose(process_isin_code),
+                             output_processor=TakeFirst())
