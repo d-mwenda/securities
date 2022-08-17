8800e6875357a452ac5ea2882a9d1cc48af98567
SKagai <sallykagai@rocketmail.com>
2022-08-17 15:57:30 +0300

    generate automatic title tag text in existing views

--- securities_web/backend/securities/index/views.py
+++ securities_web/backend/securities/index/views.py
@@ -3,7 +3,18 @@ from django.views import View
 
 
 class HomeView(View):
-    template_name ="index/home.html"
+    template_name = "index/home.html"
+    title_tag = "African Stock Markets | Share Price & Financials"
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, context=self.get_context())
+
+    def get_context(self, **kwargs):
+        ctx = kwargs
+        ctx["title_tag"] = self.title_tag
+        return ctx
+
+
+class RobotsView(View):
+    def get(self, request):
+        pass
